b6c6efb97acb66260107283a411989b1e990129c
bvandermeersch <bvandermeersch@gmail.com>
2018-09-26 22:18:06 -0500

    fix unit test

--- hyperspace_test.py
+++ hyperspace_test.py
@@ -1,10 +1,10 @@
 import unittest
-from hyperspacepy import Hyperspace
+from hyperspace import Hyperspace
 
 class HyperspaceTest(unittest.TestCase):
     def test_get_version(self):
         hyperspace = Hyperspace()
-        self.assertEqual(hyperspace.get_version(), '1.0.4')
+        self.assertEqual(hyperspace.get_version(), '0.2.0')
 
 if __name__ == '__main__':
     unittest.main()
